bd28a003eeaef95d46b2f071a387a8aa8b6a3bb3
MichalMironski <54716594+MichalMironski@users.noreply.github.com>
2019-10-20 18:15:23 +0200

    Add files via upload

---
+++ EnumTest.java
@@ -0,0 +1,16 @@
+package enums;
+
+/**
+ * Ten program demonstruje typy wyliczeniowe
+ * @author Michał
+ * @version 1.0 2019-10-20
+ */
+public class EnumTest
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System
+
+    }
+}
